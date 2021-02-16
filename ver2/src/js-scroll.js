7622259f27f7e67c5e734eab83943968e242d7e1
kimjihyunKR <kkkk6810@on.mjc.ac.kr>
2021-02-16 18:16:47 +0900

    210216

--- ver2/src/js-scroll.js
+++ ver2/src/js-scroll.js
@@ -1,11 +1,12 @@
-const secWebvr = document.querySelector("#sec-webvr");
+const secVisionWorld = document.querySelector("#sec-vision-world");
 const secAbout = document.querySelector("#sec-about");
 const secGame = document.querySelector("#sec-games");
 console.log("a")
 window.addEventListener("scroll", () => {
-  const secWebvrY = secWebvr.scrollHeight;
+  const pointUp = secVisionWorld.offsetHeight;
 
-  if(window.scrollY > secWebvrY && window.scrollY<secGame.scrollHeight){
+  if( pointUp-50 < window.scrollY && window.scrollY < pointUp+50){
     console.log("Aa")
+    window.scrollTo("sec-vision-world");
   }
 })
